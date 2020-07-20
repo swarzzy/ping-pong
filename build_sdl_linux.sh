d50ba182e999d3fc05f8c4d010c46c3fbb811ebf
swarzzy <hbr.tzuf@yandex.ru>
2020-07-20 20:07:09 +0700

    Fix SDL build for Linux

--- build_sdl_linux.sh
+++ build_sdl_linux.sh
@@ -4,6 +4,6 @@
 # Build script for Linux using clang     #
 ##########################################
 
-CXX=clang++ CC=clang cmake ext/SDL2-2.0.12/ -B./build/SDL/ -G"Unix Makefiles" -DSDL_STATIC=ON -DSDL_SHARED=OFF -DFORCE_STATIC_VCRT=ON
+CXX=clang++ CC=clang cmake ext/SDL2-2.0.12/ -B./build/SDL/ -G"Unix Makefiles" -DSDL_STATIC=ON -DSDL_SHARED=OFF -DFORCE_STATIC_VCRT=ON -DVIDEO_X11=ON
 cmake --build build/SDL
 cp build/SDL/libSDL2.a build/libSDL2.a
